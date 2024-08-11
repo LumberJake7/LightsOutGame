7b41c21943eb6d4f9b3b83390d96836fe789abc5
LumberJake7 <jakelee631@gmail.com>
2024-08-11 17:04:32 -0400

    first commit

---
+++ src/Board.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Cell from "./Cell";
+import "./Board.css";
+
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.25 }) {
+  const [board, setBoard] = useState(createBoard());
+  const [gameWon, setGameWon] = useState(false); // Renamed from hasWon to gameWon
+
+  function createBoard() {
+    let initialBoard = [];
+    for (let y = 0; y < nrows; y++) {
+      let row = [];
+      for (let x = 0; x < ncols; x++) {
+        row.push(Math.random() < chanceLightStartsOn);
+      }
+      initialBoard.push(row);
+    }
+    return initialBoard;
+  }
+
+  function checkGameWon() {
+    // Renamed from hasWon to checkGameWon
+    return board.every((row) => row.every((cell) => !cell));
+  }
+
+  function flipCellsAround(coord) {
+    setBoard((oldBoard) => {
+      const [y, x] = coord.split("-").map(Number);
+      const boardCopy = oldBoard.map((row) => [...row]);
+
+      const flipCell = (y, x) => {
+        if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+          boardCopy[y][x] = !boardCopy[y][x];
+        }
+      };
+
+      flipCell(y, x);
+      flipCell(y, x - 1);
+      flipCell(y, x + 1);
+      flipCell(y - 1, x);
+      flipCell(y + 1, x);
+
+      return boardCopy;
+    });
+    setGameWon(checkGameWon());
+  }
+
+  if (gameWon) {
+    return <div className="winner">You Won!</div>;
+  }
+
+  let tableBoard = [];
+  for (let y = 0; y < nrows; y++) {
+    let row = [];
+    for (let x = 0; x < ncols; x++) {
+      let coord = `${y}-${x}`;
+      row.push(
+        <Cell
+          key={coord}
+          isLit={board[y][x]}
+          flipCellsAroundMe={() => flipCellsAround(coord)}
+        />
+      );
+    }
+    tableBoard.push(<tr key={y}>{row}</tr>);
+  }
+
+  return (
+    <table className="Board">
+      <tbody>{tableBoard}</tbody>
+    </table>
+  );
+}
+
+export default Board;
